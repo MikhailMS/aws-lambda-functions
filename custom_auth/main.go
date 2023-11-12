6443220b53102c253710aaeeb8916ee44f3d292e
MikhailMS <1488maiklm@gmail.com>
2023-11-12 02:30:50 +0000

    Custom Auth Lambda is almost done, trying to resolve issues with token sign/verify

--- custom_auth/main.go
+++ custom_auth/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"net/http"
+  "context"
+  "errors"
+  "fmt"
 
-  "github.com/lestrrat-go/jwx/jwt"
-  "github.com/lestrrat-go/jwx/jwa"
+  "github.com/golang-jwt/jwt/v5"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,16 +14,28 @@ import (
 var (
   // secret to decode/parse JWT token would be set at build time
   TokenSecret string
-
-  // algorithm used to encode/decode JWT token
-  TokenAlg = jwa.RS256
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
 )
+
+
+// Help function to generate an IAM policy
+func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId, UsageIdentifierKey: ""}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+
+	return authResponse
+}
 
 func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
   // Step 1. Get Authorization token from Request
@@ -32,44 +43,37 @@ func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest
 	// token := event.AuthorizationToken[7:bounds]
 	token := event.AuthorizationToken
 
-  // Step 2. Validate token
-	parsedToken, err := jwt.Parse([]byte(token), jwt.WithKey(TokenAlg, TokenSecret), jwt.WithValidate(true))
+  if len(token) == 0 {
+    return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+  }
 
-  // Step 3. If token invalid, return DENY reponse
-  //         otherwises return ALLOW
+  // Step 2. Parse token
+  parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+    _, ok := token.Method.(*jwt.SigningMethodHMAC)
+    if !ok {
+      return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+    }
+    return "", nil
+  })
   if err != nil {
-
-		return events.APIGatewayCustomAuthorizerResponse{
-			PrincipalID: parsedToken.Get("principalID"),
-			PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-				Version: "2012-10-17",
-				Statement: []events.IAMPolicyStatement{
-					{
-						Action:   []string{"execute-api:Invoke"},
-						Effect:   "Deny",
-						Resource: []string{"*"},
-					},
-				},
-			},
-			UsageIdentifierKey: "",
-		}, err
+    return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 	}
 
-	return events.APIGatewayCustomAuthorizerResponse{
-    PrincipalID: parsedToken.Get("principalID"),
-		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{
-				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   "Allow",
-					Resource: []string{event.MethodArn},
-				},
-			},
-		},
-		UsageIdentifierKey: "",
-	}, nil
+  // Step 3. If token invalid, return DENY reponse
+  if !parsedToken.Valid {
+    return generatePolicy("", "Deny", "*"), err
 
+  }
+
+  // Step 4. At this point token is Valid, so we need to get principal out and return ALLOW response
+  claims, ok := parsedToken.Claims.(jwt.MapClaims)
+  if ok {
+    principal := claims["principalID"].(string)
+    return generatePolicy(fmt.Sprintf("%v", principal), "Allow", event.MethodArn), nil
+  }
+
+  // This is only called if everything is great, but principalID is not encoded into token
+  return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 }
 
 func main() {
--- custom_auth/main_test.go
+++ custom_auth/main_test.go
@@ -1,10 +1,99 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+  // "fmt"
+  "errors"
+  "context"
+  "testing"
 
-// 	"github.com/aws/aws-lambda-go/events"
-// )
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/golang-jwt/jwt/v5"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+  TokenSecret = "secret"
+
+	t.Run("Empty Token", func(t *testing.T) {
+
+    _, err := handler(context.Background(), events.APIGatewayCustomAuthorizerRequest{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+    })
+
+		if err == nil {
+			t.Fatal("Error failed to trigger with an invalid request")
+		}
+
+    assert.Equal(t, errors.New("Error: Invalid token"), err)
+	})
+
+	// t.Run("Invalid Token", func(t *testing.T) {
+    // expectedResponse := events.APIGatewayCustomAuthorizerResponse{
+      // PrincipalID: "",
+      // PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+        // Version: "2012-10-17",
+        // Statement: []events.IAMPolicyStatement{
+          // {
+            // Action:   []string{"execute-api:Invoke"},
+            // Effect:   "Deny",
+            // Resource: []string{"*"},
+          // },
+        // },
+      // },
+      // UsageIdentifierKey: "",
+    // }
+
+    // response, err := handler(context.Background(), events.APIGatewayCustomAuthorizerRequest{
+      // Type:               "TOKEN",
+      // AuthorizationToken: "invalid_token",
+      // MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+    // })
+	// 	if err == nil {
+	// 		t.Fatal("Error failed to trigger with an invalid request")
+	// 	}
+
+    // assert.Equal(t, expectedResponse, response)
+	// })
+
+	t.Run("Valid Token", func(t *testing.T) {
+    // Create valid token and sign it
+    valid_token := jwt.New(jwt.SigningMethodHS256)
+
+    claims := valid_token.Claims.(jwt.MapClaims)
+    claims["principalID"] = "test_user"
+
+    signed_token, err := valid_token.SignedString([]byte(TokenSecret))
+    if err != nil {
+      t.Fatal(err, TokenSecret, []byte(TokenSecret))
+    }
+
+    // Create expectedResponse
+    expectedResponse := events.APIGatewayCustomAuthorizerResponse{
+      PrincipalID: "test_user",
+      PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+        Version: "2012-10-17",
+        Statement: []events.IAMPolicyStatement{
+          {
+            Action:   []string{"execute-api:Invoke"},
+            Effect:   "Allow",
+            Resource: []string{"arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test"},
+          },
+        },
+      },
+      UsageIdentifierKey: "",
+    }
+
+    response, err := handler(context.Background(), events.APIGatewayCustomAuthorizerRequest{
+      Type:               "TOKEN",
+      AuthorizationToken: signed_token,
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+    })
+		if err != nil {
+      t.Fatal("Everything should be ok; ", err, "; Signed Token: ", signed_token)
+		}
+
+    assert.Equal(t, expectedResponse, response)
+	})
+}
