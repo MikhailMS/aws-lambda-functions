bb92e78ed09c9baf9291a61382c398bb1d7b357c
MikhailMS <1488maiklm@gmail.com>
2023-11-18 18:15:06 +0000

    Add new lambda function - custom auth v2 to support newer Gateway Auth; update Action pipeline to test/build/push custom_auth_v2

---
+++ custom_auth_v2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+  "context"
+  "errors"
+  "fmt"
+
+  "github.com/golang-jwt/jwt/v5"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+  // secret to decode/parse JWT token would be set at build time
+  TokenSecret string
+)
+
+
+// Help function to generate an IAM policy
+func generatePolicy(principalId, effect, resource string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
+	authResponse := events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{PrincipalID: principalId}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+
+	return authResponse
+}
+
+func handler(ctx context.Context, event events.APIGatewayV2CustomAuthorizerV1Request) (events.APIGatewayV2CustomAuthorizerIAMPolicyResponse, error) {
+  // Step 1. Get Authorization token from Request
+  // bounds := len(event.AuthorizationToken)
+	// token := event.AuthorizationToken[7:bounds]
+	// token := event.AuthorizationToken
+  token, ok := event.Headers["Authorization"]
+  
+  if !ok {
+    return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}, errors.New("Error: Invalid auth token")
+  }
+
+  if len(token) == 0 {
+    return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}, errors.New("Error: Invalid auth token")
+  }
+
+  // Step 2. Parse token
+  parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+    _, ok := t.Method.(*jwt.SigningMethodHMAC)
+    if !ok {
+      return nil, errors.New(fmt.Sprintf("Unexpected signing method: %v", t.Header["alg"]))
+    }
+    
+    return []byte(TokenSecret), nil
+  })
+  if err != nil {
+    return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}, err
+	}
+
+  // Step 3. If token invalid, return DENY reponse
+  if !parsedToken.Valid {
+    return generatePolicy("", "Deny", "*"), err
+  }
+
+  // Step 4. At this point token is Valid, so we need to get principal out and return ALLOW response
+  claims, ok := parsedToken.Claims.(jwt.MapClaims)
+  if !ok {
+    return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}, errors.New("Error: Failed to parse claims")
+  }
+
+  principal, ok := claims["principalID"].(string)
+  if ok {
+    return generatePolicy(principal, "Allow", event.MethodArn), nil
+  }
+
+  // This is only called if everything is great, but principalID is not encoded into token
+  return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}, errors.New("Error: Missing claims")
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
---
+++ custom_auth_v2/main_test.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+  "errors"
+  "context"
+  "testing"
+  "time"
+
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/golang-jwt/jwt/v5"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+  TokenSecret = "secret"
+
+	t.Run("Empty Token", func(t *testing.T) {
+
+    headers := make(map[string]string)
+    headers["Authorization"] = ""
+
+    _, err := handler(context.Background(), events.APIGatewayV2CustomAuthorizerV1Request{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+      Headers:            headers,
+    })
+
+		if err == nil {
+			t.Fatal("Error failed to trigger with an invalid request")
+		}
+
+    assert.Equal(t, errors.New("Error: Invalid auth token"), err)
+	})
+
+	t.Run("Incorrect Token", func(t *testing.T) {
+    headers := make(map[string]string)
+    headers["Authorization"] = "invalid_token"
+
+    expectedResponse := events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}
+    expectedError    := "token is malformed: token contains an invalid number of segments"
+
+    response, err := handler(context.Background(), events.APIGatewayV2CustomAuthorizerV1Request{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+      Headers:            headers,
+    })
+		if err == nil {
+			t.Fatal("Error failed to trigger with an invalid request")
+		}
+
+    assert.Equal(t, expectedResponse, response)
+    assert.Equal(t, expectedError, err.Error())
+	})
+
+	t.Run("Valid Token", func(t *testing.T) {
+    // Create valid token and sign it
+    valid_token := jwt.New(jwt.SigningMethodHS256)
+
+    claims := valid_token.Claims.(jwt.MapClaims)
+    claims["principalID"] = "test_user"
+
+    signed_token, err := valid_token.SignedString([]byte(TokenSecret))
+    if err != nil {
+      t.Fatal(err, TokenSecret, []byte(TokenSecret))
+    }
+
+    // Create expectedResponse
+    expectedResponse := events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{
+      PrincipalID: "test_user",
+      PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+        Version: "2012-10-17",
+        Statement: []events.IAMPolicyStatement{
+          {
+            Action:   []string{"execute-api:Invoke"},
+            Effect:   "Allow",
+            Resource: []string{"arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test"},
+          },
+        },
+      },
+    }
+
+    headers := make(map[string]string)
+    headers["Authorization"] = signed_token
+
+    response, err := handler(context.Background(), events.APIGatewayV2CustomAuthorizerV1Request{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+      Headers:            headers,
+    })
+		if err != nil {
+      t.Fatal("Everything should be ok; ", err, "; Signed Token: ", signed_token)
+		}
+
+    assert.Equal(t, expectedResponse, response)
+	})
+
+	t.Run("Valid Token, Missing Claim", func(t *testing.T) {
+    expectedError    := "Error: Missing claims"
+
+    // Create valid token and sign it
+    valid_token := jwt.New(jwt.SigningMethodHS256)
+
+    signed_token, err := valid_token.SignedString([]byte(TokenSecret))
+    if err != nil {
+      t.Fatal(err, TokenSecret, []byte(TokenSecret))
+    }
+
+    headers := make(map[string]string)
+    headers["Authorization"] = signed_token
+
+    // Create expectedResponse
+    expectedResponse := events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}
+
+    response, err := handler(context.Background(), events.APIGatewayV2CustomAuthorizerV1Request{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+      Headers:            headers,
+    })
+		if err == nil {
+      t.Fatal("Everything should be ok; ", err, "; Signed Token: ", signed_token)
+		}
+
+    assert.Equal(t, expectedResponse, response)
+    assert.Equal(t, expectedError, err.Error())
+	})
+
+  t.Run("Valid Token, Expired", func(t *testing.T) {
+    // Create invalid token and sign it
+    expired_token := jwt.New(jwt.SigningMethodHS256)
+
+    claims := expired_token.Claims.(jwt.MapClaims)
+    claims["exp"] = time.Now().Add(-1 * time.Hour).Unix()
+    claims["iat"] = time.Now().Add(-2 * time.Hour).Unix()
+    claims["principalID"] = "test_user"
+
+    signed_expired_token, err := expired_token.SignedString([]byte(TokenSecret))
+    if err != nil {
+      t.Fatal(err, TokenSecret, []byte(TokenSecret))
+    }
+
+    headers := make(map[string]string)
+    headers["Authorization"] = signed_expired_token
+
+    expectedResponse := events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{}
+    expectedError    := "token has invalid claims: token is expired"
+
+    response, err := handler(context.Background(), events.APIGatewayV2CustomAuthorizerV1Request{
+      Type:               "TOKEN",
+      AuthorizationToken: "",
+      MethodArn:          "arn:aws:execute-api:eu-west-2:123456789012:/test/POST/test",
+      Headers:            headers,
+    })
+		if err == nil {
+			t.Fatal("Error failed to trigger with an invalid request")
+		}
+
+    assert.Equal(t, expectedResponse, response)
+    assert.Equal(t, expectedError, err.Error())
+	})
+}
